440ab1b06b4ce1d7d22966683e6f0c44a13a5a3d
Darragh Ferry <darraghferry@hotmail.com>
2019-11-15 08:43:47 +0000

    character list component makes axios call when mounted and display some data from the response

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,30 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [chars, setChars] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(res => {
+        console.log(res);
+        setChars(...chars, res.data.results);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>List of Characters</h2>
+      {chars
+        ? chars.map(char => {
+            return <div>{char.name}</div>;
+          })
+        : "no characters"}
     </section>
   );
 }
